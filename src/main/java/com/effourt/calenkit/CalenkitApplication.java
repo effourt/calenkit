5902351585a9db9a54745445f7cc28f07c161526
jaeheela <jhla456@kakao.com>
2023-04-13 17:47:57 +0900

    first project

---
+++ src/main/java/com/effourt/calenkit/CalenkitApplication.java
@@ -0,0 +1,13 @@
+package com.effourt.calenkit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalenkitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalenkitApplication.class, args);
+	}
+
+}
