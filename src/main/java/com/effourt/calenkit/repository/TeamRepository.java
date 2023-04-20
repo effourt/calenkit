984b5e7651801d8d7a585033dc4aa4f07fc568b5
mu-aa <offlmuaa@gmail.com>
2023-04-20 22:00:49 +0900

    [Feat]MyScheduleService, TeamRepository 즐겨찾기 기능 추가

--- src/main/java/com/effourt/calenkit/repository/TeamRepository.java
+++ src/main/java/com/effourt/calenkit/repository/TeamRepository.java
@@ -11,5 +11,6 @@ public interface TeamRepository {
     Integer update(Team team);
     Integer delete(Integer teamSno, String teamMid);
     List<Team> findByMid(String teamMid);
-    List<TeamMember> findBySno(String teamSno);
+    List<Team> findByBookmark(String teamMid);
+    List<TeamMember> findBySno(Integer teamSno);
 }
