0b090810ef8666eab6bfdcad9ab7d6304f32ff8c
BeefCutlet <whysabi492@naver.com>
2023-04-21 17:57:33 +0900

    [FEAT] EmailSend.java 이메일 발송 클래스에 발송 기능 추가 build.gradle 이메일 라이브러리 추가 application.properties 이메일 라이브러리 설정 추가

--- src/main/java/com/effourt/calenkit/util/EmailSend.java
+++ src/main/java/com/effourt/calenkit/util/EmailSend.java
@@ -1,4 +1,45 @@
 package com.effourt.calenkit.util;
 
+import com.effourt.calenkit.dto.EmailMessage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import javax.servlet.http.HttpSession;
+import java.util.UUID;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
 public class EmailSend {
+
+    private final JavaMailSender javaMailSender;
+
+    public void sendMail(EmailMessage emailMessage) {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+
+        try {
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, false, "UTF-8");
+            mimeMessageHelper.setTo(emailMessage.getRecipient());
+            mimeMessageHelper.setSubject(emailMessage.getSubject());
+            mimeMessageHelper.setText(emailMessage.getMessage());
+            javaMailSender.send(mimeMessage);
+        } catch (MessagingException e) {
+            log.error("mail send error");
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String createAccessCode(String id, HttpSession session) {
+        String accessCode = UUID.randomUUID().toString();
+        session.setAttribute(accessCode, id + "ACCESS");
+        session.setMaxInactiveInterval(300);
+        return accessCode;
+    }
+
 }
