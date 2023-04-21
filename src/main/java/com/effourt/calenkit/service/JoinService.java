3f4d78c2da226a7e003e7da9aa14c85db4478dfa
BeefCutlet <whysabi492@naver.com>
2023-04-21 17:59:09 +0900

    [FEAT] JoinService.java 이메일로 회원가입 기능 추가 LoginService.java 회원가입 여부 검증 기능 메서드 추가, 메일로 로그인/회원가입 코드 전송 메서드 추가

--- src/main/java/com/effourt/calenkit/service/JoinService.java
+++ src/main/java/com/effourt/calenkit/service/JoinService.java
@@ -1,9 +1,23 @@
 package com.effourt.calenkit.service;
 
+import com.effourt.calenkit.domain.Member;
+import com.effourt.calenkit.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class JoinService {
+
+    private final MemberRepository memberRepository;
+
     //[email로 회원가입 행위] : joinByEmail()
     // => EmailSend
     // => MemberRepository.save
+    // **** 필수 정보 입력 처리 요망 ****
+    public void joinByEmail(Member member) {
+        memberRepository.save(member);
+    }
 
     //[sns로 회원가입 행위] : joinBySNS()
     // => MemberRepository.save
--- src/main/java/com/effourt/calenkit/service/LoginService.java
+++ src/main/java/com/effourt/calenkit/service/LoginService.java
@@ -1,10 +1,63 @@
 package com.effourt.calenkit.service;
 
+import com.effourt.calenkit.domain.Member;
+import com.effourt.calenkit.dto.EmailMessage;
+import com.effourt.calenkit.repository.MemberRepository;
+import com.effourt.calenkit.util.EmailSend;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
 public class LoginService {
-    // [email로 로그인 행위] : loginByEmail()
+
+    private final MemberRepository memberRepository;
+    private final EmailSend emailSend;
+
+    public Member getMemberById(String id) {
+        return memberRepository.findByMemId(id);
+    }
+
+    /**[email로 로그인 행위] : loginByEmail() */
     // => MemberRepository.find
     // => EmailSend
     // => 세션 값 저장
+
+    // [이메일 로그인] 아이디/비밀번호 존재 여부 확인
+    public String checkMember(String memberId) {
+        Member member = memberRepository.findByMemId(memberId);
+        String loginType = "";
+        if (member != null) {
+            if (member.getMemPw() != null) {
+                //아이디 존재 O, 비밀번호 존재 O
+                loginType = "PASSWORD_LOGIN";
+            } else {
+                //아이디 존재 O, 비밀번호 존재 X
+                //로그인 코드 생성 및 메일 전송
+                loginType = "CODE_LOGIN";
+            }
+        } else {
+            //아이디 존재 X, 비밀번호 존재 X
+            //회원가입 코드 생성 및 메일 전송
+            loginType = "JOIN_LOGIN";
+        }
+
+        return loginType;
+    }
+
+    // 로그인 or 회원가입 코드를 생성하고 세션에 저장
+    // 세션키를 쿠키값으로 하는 쿠키를 생성하고 쿠키명을 클라이언트의 메일로 전송
+    // 세션에 저장된 ACCESS KEY : 아이디(이메일) + ACCESS
+    public void sendCode(String id, EmailMessage emailMessage, HttpSession session) {
+        //로그인/회원가입 코드 생성
+        emailSend.createAccessCode(id, session);
+
+        //메일 전송
+        emailSend.sendMail(emailMessage);
+    }
 
     //[sns로 로그인 행위] : loginBySNS()
     // => MemberRepository.save
