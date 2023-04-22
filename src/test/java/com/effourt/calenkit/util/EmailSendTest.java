af7ad6170f3bbf014369b0280daf5532cae5ae11
BeefCutlet <whysabi492@naver.com>
2023-04-22 13:20:51 +0900

    [TEST] EmailSendTest.java 이메일 전송 테스트 코드 추가

---
+++ src/test/java/com/effourt/calenkit/util/EmailSendTest.java
@@ -0,0 +1,31 @@
+package com.effourt.calenkit.util;
+
+import com.effourt.calenkit.dto.EmailMessage;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class EmailSendTest {
+
+    @Autowired
+    EmailSend emailSend;
+
+    @Test
+    void sendMail() {
+        EmailMessage emailMessage = EmailMessage.builder()
+                .recipient("")
+                .subject("테스트")
+                .message("메일 전송 테스트")
+                .build();
+
+        emailSend.sendMail(emailMessage);
+    }
+
+}
