2013ae1cfffe6bd74e429c6746de2ce6c4150b8c
jaeheela <jhla456@kakao.com>
2023-04-23 20:57:51 +0900

    [DOCS] TeamMapper 생성

--- src/main/java/com/effourt/calenkit/controller/TeamController.java
+++ src/main/java/com/effourt/calenkit/controller/TeamController.java
@@ -1,7 +1,5 @@
 package com.effourt.calenkit.controller;
 
-import com.effourt.calenkit.domain.Member;
-import com.effourt.calenkit.domain.Team;
 import com.effourt.calenkit.dto.TeamMember;
 import com.effourt.calenkit.repository.MemberRepository;
 import com.effourt.calenkit.repository.TeamRepository;
@@ -14,29 +12,32 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/teams")
+@RequestMapping("/team")
 public class TeamController {
 
     private final TeamScheduleService teamScheduleService;
     private final TeamRepository teamRepository;
 
-    //[GET] : http://localhost:8080/teams/{}
-//    @GetMapping
-    public String showTeamPopup(){
-        return "teamPopup";
-    }
+    // 일정 상세에서 공유버튼 클릭 시 호출될 요청처리 메소드
+    // 공유 폼 뷰 페이지 요청
+    //[GET] http://localhost:8080/team
+    @GetMapping
+    public String showTeam(@RequestParam int scNo, Model model){
+        List<TeamMember> teamMemberList = teamRepository.findBySno(scNo);
+        model.addAttribute("team")
 
-    //[POST] : http://localhost:8080/teams
-//    @PostMapping
-    public String searchTeam(){
-        //teamRepository.findBySno(scNo);
         return "teamPopup";
     }
 
     //
+    //[POST] http://localhost:8080/team
+    @PostMapping
+    public String addTeam(@RequestParam int scNo){
+        teamRepository.findBySno(scNo);
+        return "teamPopup";
+    }
 }
