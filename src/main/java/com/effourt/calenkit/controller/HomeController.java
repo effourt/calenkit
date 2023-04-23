67ce15fed3d855e4ebcc5d1e7b6afb71eac0001a
jaeheela <jhla456@kakao.com>
2023-04-23 20:58:44 +0900

    [STYLE]url주소 주석 추가

--- src/main/java/com/effourt/calenkit/controller/HomeController.java
+++ src/main/java/com/effourt/calenkit/controller/HomeController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+
+    //http://localhost:8080/
     @GetMapping("/")
     public String home(){
         return "main";
