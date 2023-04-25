0d678eeabc729d66756b2814b04b1e7032b38b67
jaeheela <jhla456@kakao.com>
2023-04-25 12:38:08 +0900

    [FIX] findBySno(Integer teamSno) 수정 - 반환값을 List<Team>으로 변경

--- src/main/java/com/effourt/calenkit/repository/TeamRepositoryImpl.java
+++ src/main/java/com/effourt/calenkit/repository/TeamRepositoryImpl.java
@@ -46,7 +46,7 @@ public class TeamRepositoryImpl implements TeamRepository {
     }
 
     @Override
-    public List<TeamMember> findBySno(Integer teamSno) {
+    public List<Team> findBySno(Integer teamSno) {
         return sqlSessionTemplate.getMapper(TeamMapper.class).findBySno(teamSno);
     }
 }
