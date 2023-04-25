d5fa4ce0a04a1866025ca5f3511b5f167bb79d63
BeefCutlet <whysabi492@naver.com>
2023-04-25 13:04:26 +0900

    [FEAT] 카카오 로그인 기능 추가 LoginService.java 카카오 로그인을 위한 로직 추가 JoinService.java 소셜 로그인 시 작동할 회원가입 로직 추가 MemberController.java 소셜 로그인 요청 메서드에 회원가입/로그인 분기 로직 추가

--- src/main/java/com/effourt/calenkit/controller/MemberController.java
+++ src/main/java/com/effourt/calenkit/controller/MemberController.java
@@ -1,8 +1,11 @@
 package com.effourt.calenkit.controller;
 
+import com.effourt.calenkit.domain.Auth;
 import com.effourt.calenkit.domain.Member;
+import com.effourt.calenkit.dto.AuthUserInfoResponse;
 import com.effourt.calenkit.dto.EmailMessage;
-import com.effourt.calenkit.dto.SocialRequest;
+import com.effourt.calenkit.dto.AccessTokenRequest;
+import com.effourt.calenkit.dto.AccessTokenResponse;
 import com.effourt.calenkit.exception.MemberNotFoundException;
 import com.effourt.calenkit.repository.MemberRepository;
 import com.effourt.calenkit.service.AdminService;
@@ -10,6 +13,7 @@ import com.effourt.calenkit.service.JoinService;
 import com.effourt.calenkit.service.LoginService;
 import com.effourt.calenkit.service.MyPageService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -17,7 +21,8 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpSession;
 import java.util.List;
 
-//@Controller
+@Slf4j
+@Controller
 @RequiredArgsConstructor
     public class MemberController {
 
@@ -31,7 +36,7 @@ import java.util.List;
     //아이디 존재 O, 비밀번호 O : PASSWORD_LOGIN
     //아이디 존재 O, 비밀번호 X : CODE_LOGIN
     //아이디 존재 X, 비밀번호 X : JOIN_LOGIN
-    @PostMapping("")
+//    @PostMapping("")
     @ResponseBody
     public String checkId(String id, HttpSession session) {
         String loginType = loginService.checkMember(id);
@@ -48,8 +53,13 @@ import java.util.List;
 
     
     /** 로그인 */
+    @GetMapping("login")
+    public String login() {
+        return "login";
+    }
+
     //패스워드로 로그인
-    @PostMapping("")
+//    @PostMapping("")
     public String loginByPassword(@ModelAttribute Member member, HttpSession session) {
         Member findMember = loginService.getMemberById(member.getMemId());
         if (findMember.getMemPw().equals(member.getMemPw())) {
@@ -59,7 +69,7 @@ import java.util.List;
     }
     
     //로그인 코드로 로그인
-    @PostMapping("")
+//    @PostMapping("")
     public String loginByCode(String id, String code, HttpSession session) {
         String loginCode = (String) session.getAttribute(code);
         if (loginCode.equals(id + "ACCESS")) {
@@ -68,16 +78,46 @@ import java.util.List;
         return "";
     }
 
-    //소셜 로그인
-    @PostMapping("/kakao-login")
-    @ResponseBody
-    public String loginByKakao(@RequestBody SocialRequest socialRequest) {
 
-        return "";
+    //소셜 로그인
+    @GetMapping("/login/kakao")
+    public String loginByKakao(@RequestParam String code, HttpSession session) {
+        log.info("code={}", code);
+        AccessTokenRequest accessTokenRequest = AccessTokenRequest.builder()
+                        .clientId("ce578a556d58e95706684b75a588e1b5")
+                        .grantType("authorization_code")
+                        .redirectUri("http://localhost:8080/login/kakao")
+                        .code(code)
+                        .build();
+        //Access 토큰 발급
+        AccessTokenResponse accessToken = loginService.getAccessToken(accessTokenRequest);
+        //Access 토큰으로 카카오 리소스 서버에서 사용자 정보 가져오기
+        AuthUserInfoResponse userInfo = loginService.getAuthUserInfo(accessToken.getAccessToken());
+        log.info("userEmail={}", userInfo.getEmail());
+        
+        //사용자 이메일이 DB에 존재하지 않으면 Access 토큰, Refresh 토큰 저장 후 회원가입 실시
+        //사용자 이메일이 DB에 존재하지만 Access 토큰, Refresh 토큰이 설정되어 있지 않으면 저장 후 로그인
+        //사용자 이메일이 DB에 존재하고 Access 토큰, Refresh 토큰이 존재하는 경우 토큰 UPDATE
+        Member member = loginService.getMemberById(userInfo.getEmail());
+        if (member == null) {
+            //사용자 이메일이 DB에 존재하지 않는 경우
+            joinService.joinBySns(userInfo, accessToken);
+        } else if (member.getMemAuthId() == 0) {
+            //사용자 이메일이 DB에 존재하지만 Access 토큰, Refresh 토큰이 설정되어 있지 않는 경우
+            Auth auth = loginService.saveToken(accessToken);
+            member.setMemAuthId(auth.getAuthId());
+            loginService.saveMember(member);
+        } else if (member.getMemAuthId() != 0) {
+            //사용자 이메일이 DB에 존재하고 Access 토큰, Refresh 토큰이 존재하는 경우
+            loginService.updateToken(member.getMemId(), member.getMemAuthId());
+        }
+
+        session.setAttribute("loginId", userInfo.getEmail());
+        return "main";
     }
 
     //회원가입 후 이메일 로그인
-    @PostMapping("")
+//    @PostMapping("")
     public String loginByJoin(String id, String code, HttpSession session, Model model) {
         String joinCode = (String) session.getAttribute(code);
         if (joinCode.equals(id + "ACCESS")) {
@@ -86,7 +126,7 @@ import java.util.List;
         return "";
     }
 
-    @GetMapping("")
+//    @GetMapping("")
     public String logout(HttpSession session) {
         session.invalidate();
         return "";
@@ -95,7 +135,7 @@ import java.util.List;
     /** 회원가입 */
     // 이메일 회원가입
     // 입력 데이터 : 아이디(이메일), 비밀번호, 닉네임, 프로필 이미지(선택)
-    @PostMapping("")
+//    @PostMapping("")
     public String join(@ModelAttribute Member member) {
         joinService.joinByEmail(member);
         return "";
