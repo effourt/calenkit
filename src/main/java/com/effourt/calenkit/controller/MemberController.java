9c894b888b5ca2b9b29ed6d3db86916a804fb949
Lee <zero1044444@naver.com>
2023-04-26 14:20:21 +0900

    [DOCS] MemberController 수정

--- src/main/java/com/effourt/calenkit/controller/MemberController.java
+++ src/main/java/com/effourt/calenkit/controller/MemberController.java
@@ -24,13 +24,13 @@ import java.util.List;
 @Slf4j
 @Controller
 @RequiredArgsConstructor
-    public class MemberController {
+public class MemberController {
 
-        private final LoginService loginService;
-        private final JoinService joinService;
-        private final MyPageService myPageService;
-        private final AdminService adminService;
-        private final MemberRepository memberRepository;
+    private final LoginService loginService;
+    private final JoinService joinService;
+    private final MyPageService myPageService;
+    private final AdminService adminService;
+    private final MemberRepository memberRepository;
 
     //DB에서 아이디 체크
     //아이디 존재 O, 비밀번호 O : PASSWORD_LOGIN
@@ -51,7 +51,7 @@ import java.util.List;
         return loginType;
     }
 
-    
+
     /** 로그인 */
     @GetMapping("login")
     public String login() {
@@ -67,7 +67,7 @@ import java.util.List;
         }
         return "";
     }
-    
+
     //로그인 코드로 로그인
 //    @PostMapping("")
     public String loginByCode(String id, String code, HttpSession session) {
@@ -84,17 +84,17 @@ import java.util.List;
     public String loginByKakao(@RequestParam String code, HttpSession session) {
         log.info("code={}", code);
         AccessTokenRequest accessTokenRequest = AccessTokenRequest.builder()
-                        .clientId("ce578a556d58e95706684b75a588e1b5")
-                        .grantType("authorization_code")
-                        .redirectUri("http://localhost:8080/login/kakao")
-                        .code(code)
-                        .build();
+                .clientId("ce578a556d58e95706684b75a588e1b5")
+                .grantType("authorization_code")
+                .redirectUri("http://localhost:8080/login/kakao")
+                .code(code)
+                .build();
         //Access 토큰 발급
         AccessTokenResponse accessToken = loginService.getAccessToken(accessTokenRequest);
         //Access 토큰으로 카카오 리소스 서버에서 사용자 정보 가져오기
         AuthUserInfoResponse userInfo = loginService.getAuthUserInfo(accessToken.getAccessToken());
         log.info("userEmail={}", userInfo.getEmail());
-        
+
         //사용자 이메일이 DB에 존재하지 않으면 Access 토큰, Refresh 토큰 저장 후 회원가입 실시
         //사용자 이메일이 DB에 존재하지만 Access 토큰, Refresh 토큰이 설정되어 있지 않으면 저장 후 로그인
         //사용자 이메일이 DB에 존재하고 Access 토큰, Refresh 토큰이 존재하는 경우 토큰 UPDATE
@@ -126,7 +126,7 @@ import java.util.List;
         return "";
     }
 
-//    @GetMapping("")
+    //    @GetMapping("")
     public String logout(HttpSession session) {
         session.invalidate();
         return "";
@@ -143,40 +143,48 @@ import java.util.List;
 
     /** 관리자 */
     // Admin
+    @GetMapping(value = "/admin")
+    public String AdminList() {
+        return "admin";
+    }
+
+
+    // Admin
     // 전체 멤버 리스트 출력 & 멤버 아이디 검색 후 리스트 출력(GET)
     // 전체 리스트 출력 시 Json형식의 text로 List객체 전달
     @GetMapping(value = "/admin_memberList")
     @ResponseBody
-    public List<Member> MemberIdList(@RequestParam String keyword) {
-        List<Member> memberList=memberRepository.findAllByMemId(keyword);
-
+    public List<Member> AdminIdList(@RequestParam(required = false) String keyword) {
+        List<Member> memberList = memberRepository.findAllByMemId(keyword);
         return memberList;
     }
 
+
     // Admin
     // 멤버 상태변경(PATCH)
-    // 로그인세션에서 아이디값을 전달받아 member_list 페이지로 이동
+    //  member_list 페이지로 이동
     @PatchMapping(value ="/admin_statusModify")
-    public String MemberModify(@RequestAttribute Member member,@RequestParam Integer keyword) throws MemberNotFoundException {
-        member.setMemStatus(keyword);
-        return "admin/member_list";
+    public String AdminModify(@ModelAttribute Member member) throws MemberNotFoundException {
+        Member selectMember=memberRepository.findByMemId(member.getMemId());
+        return "adminPage";
     }
 
     // Admin
-    // 멤버 삭제(PATCH)
+    // 멤버 삭제(DELETE)
     // 로그인세션에서 아이디값을 전달받아 member_list 페이지로 이동
     @DeleteMapping(value ="/admin_delete")
-    public String MemberModify(@RequestAttribute Member member) throws MemberNotFoundException {
+    public String AdminDelete(@ModelAttribute Member member) throws MemberNotFoundException {
         adminService.removeMember(member);
-        return "admin/member_list";
+        return "adminPage";
     }
 
     /** 마이 페이지 */
     // MyPage
     // 멤버 닉네임 검색 후 중복 확인(GET)
+    // Ajax 처리를 위해 닉네임 값 반환
     @GetMapping(value = "/myPage_name")
     @ResponseBody
-    public String MemberName(@RequestParam String memName) {
+    public String MyPageName(@RequestParam String memName) {
         Member member=memberRepository.findByMemName(memName);
         return member.getMemName();
     }
@@ -185,31 +193,42 @@ import java.util.List;
     // 멤버 정보변경(GET)
     // 로그인세션에서 아이디값을 전달받아 member_modify 페이지로 이동
     @GetMapping(value ="/myPage_modify")
-    public String MemberModify(Model model, HttpSession session) throws MemberNotFoundException {
+    public String MyPageModify(Model model, HttpSession session) throws MemberNotFoundException {
         Member member=(Member)session.getAttribute("loginMember");
         model.addAttribute("member", memberRepository.findByMemId(member.getMemId()));
-        return "member/member_modify";
+        return "myPage";
     }
+
 
     // MyPage
     // 멤버 정보변경(PATCH)
     // Form태그를 통해 전달받은 값들을 member 객체에 update 후 member_modify 페이지로 이동
-    //    @PatchMapping(value ="/myPage_modify")
-    //    public String MemberModify(HttpSession session) throws MemberNotFoundException {
-    //        Member member=(Member)session.getAttribute("loginMember");
-    //        myPageService.modifyMe(member);
-    //        return "redirect:/member_modify";
-    //    }
-    //
-    //
-    //    // MyPage
-    //    // 멤버 비밀번호 정보변경(PATCH)
-    //    // 로그인세션에서 아이디값을 전달받아 member_modify 페이지로 이동처리.
-    @PatchMapping(value ="/myPage_pwModify")
-    public String MemberPwModify(Model model, HttpSession session) throws MemberNotFoundException {
+    @PatchMapping(value ="/myPage_modify")
+    public String MyPageModify(HttpSession session) throws MemberNotFoundException {
+        Member member=(Member)session.getAttribute("loginMember");
+        myPageService.modifyMe(member);
+
+        return "redirect:myPage";
+    }
+
+
+    // MyPage
+    // 멤버 비밀번호 정보변경(Put)
+    // 로그인세션에서 아이디값을 전달받아 member_pwModify 페이지로 이동처리.
+    @PutMapping(value ="/myPage_pwModify")
+    public String MyPagePwModify(HttpSession session) throws MemberNotFoundException {
         Member member=(Member)session.getAttribute("loginMember");
         myPageService.modifyPassword(member);
-        return "member/member_pwModify";
+        return "myPage";
+    }
+    // MyPage
+    // 멤버 상태 변경(Put)
+    // 로그인세션에서 아이디값을 전달받아 member_delete 페이지로 이동처리.
+    @PutMapping(value ="/myPage_delete")
+    public String MyPageDelete(HttpSession session) throws MemberNotFoundException {
+        Member member=(Member)session.getAttribute("loginMember");
+        myPageService.removeMe(member);
+        return "myPage";
     }
 
 }
