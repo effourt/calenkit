0ee6e1f530b1ebc0ce0011b711b1d46d8c99a151
BeefCutlet <whysabi492@naver.com>
2023-04-26 13:01:02 +0900

    [ETC] @EnableFeignClients 위치 변경

--- src/main/java/com/effourt/calenkit/CalenkitApplication.java
+++ src/main/java/com/effourt/calenkit/CalenkitApplication.java
@@ -2,8 +2,10 @@ package com.effourt.calenkit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = {"com.effourt.calenkit.client"})
 public class CalenkitApplication {
 
 	public static void main(String[] args) {
