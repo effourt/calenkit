a7046eb8d6e84809fb4689fa4b699deb89b6d310
BeefCutlet <whysabi492@naver.com>
2023-04-26 13:04:44 +0900

    [FEAT] KakaoFeignClient.java 카카오 리소스 서버 요청 메서드 삭제

--- src/main/java/com/effourt/calenkit/client/KakaoFeignClient.java
+++ src/main/java/com/effourt/calenkit/client/KakaoFeignClient.java
@@ -3,12 +3,16 @@ package com.effourt.calenkit.client;
 import com.effourt.calenkit.config.FeignConfig;
 import com.effourt.calenkit.dto.AccessTokenResponse;
 import com.effourt.calenkit.dto.AuthUserInfoResponse;
+import feign.Headers;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
 
 @FeignClient(name = "kakaoAuth", url = "https://kauth.kakao.com", configuration = {FeignConfig.class})
 public interface KakaoFeignClient {
@@ -20,7 +24,4 @@ public interface KakaoFeignClient {
             @RequestParam("redirect_uri") String redirectUri,
             @RequestParam("code") String authorizationCode
     );
-
-    @PostMapping("/v2/user/me")
-    AuthUserInfoResponse getAuthUserInfo(@RequestHeader("Authorization") String authorization);
 }
