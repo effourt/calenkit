3a6c635dbd9d89fdf393ac9f4efd1f4663571332
BeefCutlet <whysabi492@naver.com>
2023-04-29 20:26:26 +0900

    [DOCS] 유저 권한 체크용 인터셉터 추가

---
+++ src/main/java/com/effourt/calenkit/util/interceptor/UserAuthInterceptor.java
@@ -0,0 +1,16 @@
+package com.effourt.calenkit.util.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+public class UserAuthInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+}
