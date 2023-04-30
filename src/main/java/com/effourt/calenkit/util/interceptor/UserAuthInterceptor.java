3a57cf611316e7fe69257d6c9d8124f126513c3d
BeefCutlet <whysabi492@naver.com>
2023-04-30 10:14:20 +0900

    [FEAT] 로그인 여부 확인 인터셉터 기능 추가

--- src/main/java/com/effourt/calenkit/util/interceptor/UserAuthInterceptor.java
+++ src/main/java/com/effourt/calenkit/util/interceptor/UserAuthInterceptor.java
@@ -5,12 +5,19 @@ import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @Slf4j
 public class UserAuthInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        return HandlerInterceptor.super.preHandle(request, response, handler);
+        HttpSession session = request.getSession();
+        String loginId = (String) session.getAttribute("loginId");
+        log.info("UserAuthInterceptor loginId={}", loginId);
+        if (loginId == null) {
+            response.sendRedirect(request.getContextPath() + "/login/form");
+        }
+        return true;
     }
 }
