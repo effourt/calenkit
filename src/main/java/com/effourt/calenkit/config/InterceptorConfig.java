198af88a15ebe7e57a8be34ffaa8b19681ee523e
BeefCutlet <whysabi492@naver.com>
2023-04-30 10:15:09 +0900

    [DOCS] UserAuthInterceptor.java를 LoginInterceptor.java로 이름 변경

--- src/main/java/com/effourt/calenkit/config/InterceptorConfig.java
+++ src/main/java/com/effourt/calenkit/config/InterceptorConfig.java
@@ -1,6 +1,6 @@
 package com.effourt.calenkit.config;
 
-import com.effourt.calenkit.util.interceptor.UserAuthInterceptor;
+import com.effourt.calenkit.util.interceptor.LoginInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,7 +10,7 @@ public class InterceptorConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new UserAuthInterceptor())
+        registry.addInterceptor(new LoginInterceptor())
                 .order(1)
                 .addPathPatterns("")
                 .excludePathPatterns("");
--- src/main/java/com/effourt/calenkit/util/interceptor/LoginInterceptor.java
+++ src/main/java/com/effourt/calenkit/util/interceptor/LoginInterceptor.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @Slf4j
-public class UserAuthInterceptor implements HandlerInterceptor {
+public class LoginInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
