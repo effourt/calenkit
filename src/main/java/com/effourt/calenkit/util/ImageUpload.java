d7ba2abc7d2abda2475951ccb9d98cedb1979152
Lee <zero1044444@naver.com>
2023-05-02 18:44:08 +0900

    [REFACTOR] 잘못된 경로 설정으로 인해 롤백

--- src/main/java/com/effourt/calenkit/util/ImageUpload.java
+++ src/main/java/com/effourt/calenkit/util/ImageUpload.java
@@ -1,8 +1,6 @@
 package com.effourt.calenkit.util;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -12,11 +10,10 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.UUID;
 
+
 @RequiredArgsConstructor
 @Service
 public class ImageUpload {
-    @Value("${image.path}")
-    private String imagePath;
     public  String uploadImage(MultipartFile file) throws IOException {
         byte[] bytes = file.getBytes();
         String originalFilename = file.getOriginalFilename();
@@ -24,7 +21,7 @@ public class ImageUpload {
         String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
         String filename = UUID.randomUUID().toString() + extension;
         //이미지가 저장될 경로
-        Path path = Paths.get("imagePath" + filename);
+        Path path = Paths.get("C:/Users/이진규/IdeaProjects/calenkit6/src/main/resources/static/img/" + filename);
         //파일 저장
         Files.write(path, bytes);
         return filename;
