45568bd44c8ec074c9d419952e0eb54d0d14b94b
BeefCutlet <whysabi492@naver.com>
2023-05-02 17:59:00 +0900

    [DOCS] 유저 권한(일반 사용자, 관리자) 체크 인터셉터 클래스 추가

---
+++ src/main/java/com/effourt/calenkit/util/interceptor/UserAuthInterceptor.java
@@ -0,0 +1,37 @@
+package com.effourt.calenkit.util.interceptor;
+
+import com.effourt.calenkit.domain.Member;
+import com.effourt.calenkit.repository.MemberRepository;
+import com.effourt.calenkit.service.LoginService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@Slf4j
+public class UserAuthInterceptor implements HandlerInterceptor {
+
+    private MemberRepository memberRepository;
+
+    public UserAuthInterceptor(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        HttpSession session = request.getSession();
+        String loginId = (String) session.getAttribute("loginId");
+        log.info("UserAuthInterceptor loginId={}", loginId);
+
+        Member findMember = memberRepository.findByMemId(loginId);
+        log.info("UserAuthInterceptor findMemberId={}, findMemberStatus={}", findMember.getMemId(), findMember.getMemStatus());
+        if (findMember.getMemStatus() == 9) {
+            response.sendRedirect("/admin");
+        }
+        return true;
+    }
+}
