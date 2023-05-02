c11399cca8fcd241dda96a45407c018503366cf2
Lee <zero1044444@naver.com>
2023-05-02 10:27:15 +0900

    [FEAT]이미지 파일 업로드 UUID 추가

---
+++ src/main/java/com/effourt/calenkit/util/ImageUpload.java
@@ -0,0 +1,28 @@
+package com.effourt.calenkit.util;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.UUID;
+
+@Service
+public class ImageUpload {
+    public  String uploadImage(MultipartFile file) throws IOException {
+
+        byte[] bytes = file.getBytes();
+        String originalFilename = file.getOriginalFilename();
+        //고유 ID값을 부여해서 이미지 이름 중복되지 않게 처리.
+        String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
+        String filename = UUID.randomUUID().toString() + extension;
+        //이미지가 저장될 경로
+        Path path = Paths.get("C:/Users/이진규/IdeaProjects/calenkit6/src/main/resources/static/img/" + filename);
+        //파일 저장
+        Files.write(path, bytes);
+        return filename;
+    }
+}
