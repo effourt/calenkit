30536567a0167565592dc33d386ef6d5ffba3f2e
jaeheela <jhla456@kakao.com>
2023-05-03 16:00:41 +0900

    [DOCS] 동행출력 시 image 함께 출력하기 위한 DTO객체 추가

---
+++ src/main/java/com/effourt/calenkit/dto/TeamShare.java
@@ -0,0 +1,12 @@
+package com.effourt.calenkit.dto;
+
+import com.effourt.calenkit.domain.Team;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TeamShare {
+    private Team team;
+    private String image;
+}
