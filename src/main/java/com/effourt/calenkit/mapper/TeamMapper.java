9bde308ce3dabb5a914b5eb799e40556a26eaa9a
jaeheela <jhla456@kakao.com>
2023-05-03 15:58:53 +0900

    [STYLE] 불필요한 import 정리

--- src/main/java/com/effourt/calenkit/mapper/TeamMapper.java
+++ src/main/java/com/effourt/calenkit/mapper/TeamMapper.java
@@ -1,7 +1,6 @@
 package com.effourt.calenkit.mapper;
 
 import com.effourt.calenkit.domain.Team;
-import com.effourt.calenkit.dto.TeamMember;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -12,7 +11,6 @@ public interface TeamMapper {
     Integer save(Team team);
     Integer update(Team team);
     Integer delete(Integer teamSno, String teamMid);
-
     List<Team> findByMid(String teamMid);
     List<Integer> findByid(String teamMid);
 
