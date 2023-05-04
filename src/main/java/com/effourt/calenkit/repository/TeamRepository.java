9ecddbed9a10a389f006ab344d2dcd69a0b17f67
JERI <107570140+jaeheela@users.noreply.github.com>
2023-05-04 15:39:23 +0900

    pull (#89)
    * [DOCS] 불필요한 teamMemberDTO 삭제
    
    * [STYLE] 불필요한 import 정리
    
    * [FEAT] insert문의 teamlevel 고정값 9에서 매개변수로 변경
    
    * [DOCS] 동행출력 시 image 함께 출력하기 위한 DTO객체 추가
    
    * [DOCS] 불필요한 ajaxStatus 삭제
    
    * [DOCS] exception 객체 추가, 내용은 미정
    
    * [FEAT] 동행 추가, 동행의 권한 변경 서비스 기능 구현 완료
    
    * [FEAT] 동행 추가, 동행의 권한 변경, 동행 삭제 서비스 기능 구현 완료
    
    * [FIX] 매개변수에 @Param 추가
    
    * [FEAT] 알람기능 수정중
    
    * [FEAT] 알람카테고리 추가
    
    * [DOCS] Naming Convention 추가
    
    * [FEAT] 동행 추가,삭제,권한변경 기능 추가
    
    * [FEAT] 동행 추가,삭제,권한변경 + 알람서비스 기능 추가
    
    * [FIX] 매개변수명 수정
    
    * [FIX] addAlarmByDeleteTeam 서비스 수정중
    
    * [FIX] addAlarmByDeleteTeam 서비스 수정중
    
    * [FIX] delete의 where 조건 수정
    
    * [FIX] 알람 삭제 서비스 수정
    
    * [FIX]즐겨찾기 일정 SELECT SQL 명령 반환형 수정
    
    * [TEST]완전삭제 임시버튼
    
    * [FEAT]일정, 즐겨찾기 리스트 출력, 완전삭제 기능 완성
    
    * [FIX]DIV 태그 안닫힌거 수정
    
    * [FEAT]휴지통 출력 기능 완성, Status에 따른 출력 설정
    
    * [FIX]조건에 만족하는 일정 없는 예외경우 처리
    
    ---------
    
    Co-authored-by: mu-aa <offlmuaa@gmail.com>

--- src/main/java/com/effourt/calenkit/repository/TeamRepository.java
+++ src/main/java/com/effourt/calenkit/repository/TeamRepository.java
@@ -11,6 +11,6 @@ public interface TeamRepository {
     Integer delete(Integer teamSno, String teamMid);
     List<Team> findByMid(String teamMid);
     List<Integer> findByid(String teamMid);
-    List<Team> findByBookmark(String teamMid);
+    List<Integer> findByBookmark(String teamMid);
     List<Team> findBySno(Integer teamSno);
 }
