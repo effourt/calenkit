b52e7e08e0c7340387a44fd2711591ab99716dd7
BeefCutlet <whysabi492@naver.com>
2023-05-04 19:27:13 +0900

    [ETC] 인터셉터 경로 설정

--- src/main/java/com/effourt/calenkit/config/InterceptorConfig.java
+++ src/main/java/com/effourt/calenkit/config/InterceptorConfig.java
@@ -1,7 +1,6 @@
 package com.effourt.calenkit.config;
 
 import com.effourt.calenkit.repository.MemberRepository;
-import com.effourt.calenkit.service.LoginService;
 import com.effourt.calenkit.util.interceptor.LoginInterceptor;
 import com.effourt.calenkit.util.interceptor.UserAuthInterceptor;
 import lombok.RequiredArgsConstructor;
@@ -13,18 +12,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @RequiredArgsConstructor
 public class InterceptorConfig implements WebMvcConfigurer {
 
-    private final MemberRepository memberRepository;
+    private final LoginInterceptor loginInterceptor;
+    private final UserAuthInterceptor userAuthInterceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         //유저 로그인 상태 체크 인터셉터
-        registry.addInterceptor(new LoginInterceptor())
+        registry.addInterceptor(loginInterceptor)
                 .order(1)
-                .addPathPatterns("")
-                .excludePathPatterns("");
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/js/**", "/img/**", "/library/**", "/login/**", "/join/**");
         //유저 권한 체크 인터셉터
-        registry.addInterceptor(new UserAuthInterceptor(memberRepository))
-                .order(2)
-                .addPathPatterns("");
+//        registry.addInterceptor(userAuthInterceptor)
+//                .order(2)
+//                .addPathPatterns("");
     }
 }
