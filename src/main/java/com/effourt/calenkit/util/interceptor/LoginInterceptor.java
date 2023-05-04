e8f240673841f96580584136f0000043ca330aa9
BeefCutlet <whysabi492@naver.com>
2023-05-04 19:28:30 +0900

    [ETC] LoginInterceptor.java 로그 수정, Bean 파일로 등록

--- src/main/java/com/effourt/calenkit/util/interceptor/LoginInterceptor.java
+++ src/main/java/com/effourt/calenkit/util/interceptor/LoginInterceptor.java
@@ -1,6 +1,7 @@
 package com.effourt.calenkit.util.interceptor;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
@@ -8,13 +9,14 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 @Slf4j
+@Component
 public class LoginInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         HttpSession session = request.getSession();
         String loginId = (String) session.getAttribute("loginId");
-        log.info("UserAuthInterceptor loginId={}", loginId);
+        log.info("LoginInterceptor loginId={}", loginId);
         if (loginId == null) {
             response.sendRedirect(request.getContextPath() + "/login/form");
         }
