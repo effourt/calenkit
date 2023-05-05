7f2cd3ada96459f439ee09af53649c628143fd31
BeefCutlet <whysabi492@naver.com>
2023-05-05 11:26:03 +0900

    [FEAT] PasswordEncoder의 구현체를 BCryptPasswordEncoder로 설정

--- src/main/java/com/effourt/calenkit/config/SpringSecurityConfig.java
+++ src/main/java/com/effourt/calenkit/config/SpringSecurityConfig.java
@@ -1,9 +1,12 @@
 package com.effourt.calenkit.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -13,5 +16,10 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic().disable();
         http.csrf().disable(); //csrf 방지 기능 비활성화 -- 테스트용 - 배포 시에 삭제할 것
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
