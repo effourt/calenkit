4ef1871aaf589f6f447432d18238d0703317f8f8
BeefCutlet <whysabi492@naver.com>
2023-05-19 16:27:20 +0900

    [FEAT] 일정 내용 웹소켓 수신 및 전송 기능(프로토타입) javascript 삭제

--- src/main/resources/static/js/websocket.js
+++ src/main/resources/static/js/websocket.js
@@ -32,38 +32,3 @@ function sendAlarm(teamSno, teamMid, alCate) {
     };
     socket.onerror = function (err) {console.log('Errors : ' , err);};
 }
-
-
-function receiveSchedule() { //웹소켓 수신
-    let socket = new SockJS('/schedule');
-    socket.onopen = function() {
-        console.log('info: connection opened.');
-    };
-    socket.onmessage = function(event) {
-        let data = event.data;
-        console.log("ReceiveMessage : " + data + "\n");
-        // 모달 알림
-        alert(data);
-        console.log(data);
-    };
-    socket.onclose = function() {
-        console.log('connect close');
-    };
-    socket.onerror = function (err) {console.log('Errors : ' , err);};
-}
-function sendSchedule(teamSno,teamMid,html) {
-    let socket = new SockJS('/schedule');
-    socket.onopen = function() {
-        console.log('info: connection opened.');
-        socket.send(html);
-    };
-    const message = {
-        "scNo":teamSno,
-        "idList":[teamMid],
-        "html":html
-    };
-    socket.onclose = function() {
-        console.log('connect close');
-    };
-    socket.onerror = function (err) {console.log('Errors : ' , err);};
-}
